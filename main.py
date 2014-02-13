9f5a1fb077801f010ec1629a0a986d1586c31046
Mihai Bivol <mm.bivol@gmail.com>
2014-02-13 17:25:41 +0200

    Add main script

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+from pylinthub.runner import review_pull_request
+from credentials import credentials
+
+USAGE = "%s repo pull_request_number"
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print USAGE % sys.argv[0]
+        sys.exit(1)
+
+    repo = sys.argv[1]
+    pull_number = int(sys.argv[2])
+
+    review_pull_request(repo, pull_number, **credentials)
