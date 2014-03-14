475387d1575592d47020ab019c30bd3e4c4d4be4
Mihai Bivol <mm.bivol@gmail.com>
2014-03-14 15:13:11 +0200

    Add base credentials module

--- credentials.py
+++ credentials.py
@@ -1,2 +1,16 @@
+"""Add github credentials here:
+
 credentials = {
+        'login_or_token': 'your_github_token'
 }
+
+or
+
+credentials = {
+        'login_or_token': 'your_github_user', 'password':'your_password'
+}
+
+The default is unauthorized
+"""
+
+credentials = { }
