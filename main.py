5129a8dcfad71c40645dcba4da9c51110732a364
Mihai Bivol <mm.bivol@gmail.com>
2014-03-17 13:12:25 +0200

    Port arguments to argparse #16

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 """Entrypoint for running the notification script"""
 import sys
 
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
 from pylinthub.runner import review_pull_request
 from credentials import credentials
 
 USAGE = "%s repo pull_request_number [pylint_rc_file]"
 
+def get_arguments():
+    """Adds commandline arguments"""
+    arg_parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+
+    arg_parser.add_argument('-r', '--repo',
+            nargs = 1,
+            required = True,
+            type = str,
+            help = 'Repository of the Pull Request')
+
+    arg_parser.add_argument('-n', '--pull_number',
+            nargs=1,
+            required=True,
+            type=int,
+            help='Pull Request number')
+
+    arg_parser.add_argument('-rc', '--rcfile',
+            nargs = 1,
+            required = False,
+            type = str,
+            help = 'Pylint rc file')
+
+    return arg_parser.parse_args(sys.argv[1:])
+
 def main():
     """Reviews a pull request with the given system arguments"""
-    if len(sys.argv) < 3 or len(sys.argv) > 4:
-        print USAGE % sys.argv[0]
-        sys.exit(1)
+    args = vars(get_arguments())
 
-    repo = sys.argv[1]
-    pull_number = int(sys.argv[2])
+    pull_number = args['pull_number'][0]
+    repo = args['repo'][0]
+    pylintrc = args['rcfile'][0] if args['rcfile'] else None
 
-    pylintrc = None
-    if len(sys.argv) == 4:
-        pylintrc = sys.argv[3]
     review_pull_request(repo, pull_number, pylintrc, **credentials)
 
 if __name__ == "__main__":
